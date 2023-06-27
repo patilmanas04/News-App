a9e9048915a82cb10bc0c4775c78f28d801996f0
Manas Patil <pmanas13092004@gmail.com>
2023-06-27 23:55:52 +0530

    Added rel='noreferrer' to the anchor tag as i was using target='_blank' attribute in react JSX

--- src/Components/NewsCard.js
+++ src/Components/NewsCard.js
@@ -10,7 +10,7 @@ class NewsCard extends Component{
                     <div className="news-card-content">
                         <h2 className="news-card-content-title" data-theme={theme}>{title?title:""}</h2>
                         <p className="news-card-content-description" data-theme={theme}>{description?description:""}</p>
-                        <a href={url} target="_blank"><button className={`read-more-btn ${url?'enable':'disable'}`}>Read More</button></a>
+                        <a rel="noreferrer" href={url} target="_blank"><button className={`read-more-btn ${url?'enable':'disable'}`}>Read More</button></a>
                     </div>
             </div>
             </>
